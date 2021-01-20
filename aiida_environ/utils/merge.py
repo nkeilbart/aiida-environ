1e5773c703391d71c91f0a877f03b245b44f8e6a
Matthew Truscott <matthew.a.truscott@gmail.com>
2021-01-20 13:15:21 -0600

    fixed bug

--- aiida_environ/utils/merge.py
+++ aiida_environ/utils/merge.py
@@ -5,9 +5,9 @@ def recursive_update_dict(d1: dict, d2: dict) -> None:
         d1 (dict): The dictionary to be updated
         d2 (dict): The dictionary to add to the updated
     """
-    for k1 in d1:
-        for k2 in d2:
-            if k2 not in d1:
-                d1[k2] = d2[k2]
-            elif k2 == k1:
-                d1[k1].update(d2[k2])
+    for k in d2:
+        if k not in d1:
+            d1[k] = d2[k]
+    for k in d1:
+        if k in d2:
+            d1[k].update(d2[k])
