3fb459ca30fd71d29582d82671e1239ef25630f5
Ajay <kunhu2004@gmail.com>
2021-01-31 21:14:09 -0600

    test 1

---
+++ aiida_environ/tests/adsorbate_test.py
@@ -0,0 +1,15 @@
+import math
+from graph_tool.all import *
+from graph_tool.correlations import combined_corr_hist
+from typing import List
+from copy import deepcopy
+import sys
+sys.path.insert(1, '../utils')
+from adsorbate_graph import adsorbate_possibilities
+
+# points_per_site = [1, 4, 1]
+# adsorbate_per_site = [3, 4, 4]
+points_per_site = [2, 2]
+adsorbate_per_site = [2, 1]
+n = len(points_per_site)
+adsorbate_possibilities(points_per_site, adsorbate_per_site)
