650b31f754ea1e0090df13ee989b41c662edfc07
Ajay <kunhu2004@gmail.com>
2021-01-31 21:44:17 -0600

    Test 1

--- aiida_environ/tests/adsorbate_test.py
+++ aiida_environ/tests/adsorbate_test.py
@@ -4,6 +4,7 @@ from graph_tool.correlations import combined_corr_hist
 from typing import List
 from copy import deepcopy
 import sys
+import os
 sys.path.insert(1, '../utils') # Run from aiida-environ/tests folder
 from adsorbate_graph import adsorbate_possibilities
 
@@ -12,4 +13,6 @@ from adsorbate_graph import adsorbate_possibilities
 points_per_site = [2, 2]
 adsorbate_per_site = [2, 1]
 n = len(points_per_site)
-adsorbate_possibilities(points_per_site, adsorbate_per_site, 2, 'hist1.svg', 'graphs/graph1.pdf')
+FOLDER = 'graphs'
+
+adsorbate_possibilities(points_per_site, adsorbate_per_site, 2, os.join(FOLDER, 'hist1.svg'), 'graphs/graph1.pdf')
