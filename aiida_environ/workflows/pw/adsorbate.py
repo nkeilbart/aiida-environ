dc54b4ad478f86b88a4e302ce0d1ce1e3e660025
Ajay <kunhu2004@gmail.com>
2021-03-19 16:55:39 -0500

    Finalized adsorbate.py

--- aiida_environ/workflows/pw/adsorbate.py
+++ aiida_environ/workflows/pw/adsorbate.py
@@ -5,29 +5,17 @@ from aiida.orm import StructureData, ArrayData, List
 import sys
 sys.path.insert(1, '../../calculations')
 from adsorbate_calc import AdsorbateCalculation
-<<<<<<< HEAD
 from aiida_quantumespresso.utils.mapping import prepare_process_inputs
 PwBaseWorkChain = WorkflowFactory('environ.pw.base')
-=======
-
-EnvPwCalculation = CalculationFactory('environ.pw')
-# TODO copy the way that relax does this: expose inputs to base workchain instead of envpwcalc - see the notes I'll send separately
->>>>>>> 3b4eda8e5f8e57acea5d34dd4693d77ee9feb312
 
 class AdsorbateSimulation(WorkChain):
     @classmethod
     def define(cls, spec):
         super().define(spec)
-<<<<<<< HEAD
         spec.expose_inputs(PwBaseWorkChain, namespace='base',
             namespace_options={'help': 'Inputs for the `PwBaseWorkChain`.'},
             exclude=('pw.structure'))
         spec.inputs('vacancies', valid_type=ArrayData)
-=======
-        spec.expose_inputs(EnvPwCalculation, namespace='pw',
-            namespace_options={'help': 'Inputs for the `EnvPwCalculation`.'})
-        spec.inputs('adsorbate_coords', valid_type=ArrayData)
->>>>>>> 3b4eda8e5f8e57acea5d34dd4693d77ee9feb312
         spec.inputs('site_index', valid_type=List) # List of ints
         spec.inputs('possible_adsorbates', valid_type=List) # List of structures
         spec.inputs('adsorbate_index', valid_type=List) # List of Lists of Ints
@@ -44,20 +32,8 @@ class AdsorbateSimulation(WorkChain):
         self.ctx.struct_list = None
 
     def selection(self):
-<<<<<<< HEAD
         self.ctx.struct_list = AdsorbateCalculation(self.inputs.site_index, self.inputs.possible_adsorbates, self.inputs.adsorbate_index, self.inputs.structure, self.inputs.vacancies)
-=======
-        inputs = AttributeDict(self.exposed_inputs(EnvPwCalculation, namespace='pw'))
-        inputs.environ_parameters = inputs.environ_parameters.get_dict()
-        use_adsorbates = AdsorbateCalculation(self.inputs.site_index, self.inputs.possible_adsorbates, self.inputs.adsorbate_index)
-        # Get the list of structures that should be used in calculations
-        self.ctx.structures = ["""INPUT STRUCTURE"""] * len(use_adsorbates)
-        for i, x in use_adsorbates:
-            for j, y in x:
-                for k, z in y:
-                    if z != 0:
-                        self.ctx.structures[i].append_atom(position=self.inputs.adsorbate_coords[j], symbols=z)
->>>>>>> 3b4eda8e5f8e57acea5d34dd4693d77ee9feb312
+
         
 
     def simulate(self):
