477d210159700048b29f8f95a7c55a70e5b27eaf
Matthew Truscott <matthewtruscott@my.unt.edu>
2021-03-18 20:41:14 -0500

    updated adsorbate.py

--- aiida_environ/workflows/pw/adsorbate.py
+++ aiida_environ/workflows/pw/adsorbate.py
@@ -14,7 +14,7 @@ class AdsorbateSimulation(WorkChain):
         super().define(spec)
         spec.expose_inputs(EnvPwCalculation, namespace='pw',
             namespace_options={'help': 'Inputs for the `EnvPwCalculation`.'})
-        spec.inputs('vacancies', valid_type=ArrayData)
+        spec.inputs('adsorbate_coords', valid_type=ArrayData)
         spec.inputs('site_index', valid_type=List) # List of ints
         spec.inputs('possible_adsorbates', valid_type=List) # List of structures
         spec.inputs('adsorbate_index', valid_type=List) # List of Lists of Ints
@@ -29,17 +29,16 @@ class AdsorbateSimulation(WorkChain):
         pass
 
     def selection(self):
-        """ FIXME: I am not sure how to go from vacancies (coords) to site_index so for now, assume every vacancy is its own site
-        """
-        site_index = [i for i, c in enumerate(self.inputs.vacancies)]
-        use_adsorbates = AdsorbateCalculation(site_index, self.inputs.possible_adsorbates, self.inputs.adsorbate_index)
+        inputs = AttributeDict(self.exposed_inputs(EnvPwCalculation, namespace='pw'))
+        inputs.environ_parameters = inputs.environ_parameters.get_dict()
+        use_adsorbates = AdsorbateCalculation(self.inputs.site_index, self.inputs.possible_adsorbates, self.inputs.adsorbate_index)
         # Get the list of structures that should be used in calculations
         self.ctx.structures = ["""INPUT STRUCTURE"""] * len(use_adsorbates)
         for i, x in use_adsorbates:
             for j, y in x:
                 for k, z in y:
                     if z != 0:
-                        self.ctx.structures[i].append_atom(position=self.inputs.vacancies[j], symbols=z)
+                        self.ctx.structures[i].append_atom(position=self.inputs.adsorbate_coords[j], symbols=z)
         
     def simulate(self):
         pass
