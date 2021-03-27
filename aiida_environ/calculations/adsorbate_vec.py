0e7d74a604db649aece23420a8cc76cbf188d2b6
Ajay <kunhu2004@gmail.com>
2021-03-27 14:00:39 -0500

    Working vector reflection

--- aiida_environ/calculations/adsorbate_vec.py
+++ aiida_environ/calculations/adsorbate_vec.py
@@ -1,6 +1,12 @@
 import numpy as np
-def vec_point_dist(vec1, vec2, vecp):
+def point_project(vec1, vec2, vecp):
     cross = np.cross(vec1, vec2)
     dot = np.dot(cross, vecp)
     cross_norm = np.sqrt(sum(cross**2))
-    return dot / cross_norm
+    return (dot / cross_norm) * (cross / cross_norm)
+
+def reflect_point(vec1, vec2, vecp):
+    cross = np.cross(vec1, vec2)
+    t = -(np.dot(cross, vecp) / np.dot(cross, cross))
+    reflected_point = vecp + 2 * t * cross
+    return reflected_point
--- tests/test_adsorbate_vec.py
+++ tests/test_adsorbate_vec.py
@@ -1,13 +1,16 @@
 import unittest
 import numpy as np
-from aiida_environ.calculations.adsorbate_vec import vec_point_dist
+from aiida_environ.calculations.adsorbate_vec import reflect_point
 class TestAdsVec(unittest.TestCase):
-    def test_easy(self):
-        # Plane of 2x - 2y + 5z + 8 = 0
-        vec1 = [0,9,2]
-        vec2 = [-9,0,2]
-        vecp = [4,-4,3]
-        self.assertEqual(11*(3**0.5)/3, vec_point_dist(vec1, vec2, vecp))
+    def test_hard(self):
+        # Plane of 9x - 7y - 5z = 0
+        vec1 = [1,2,-1]
+        vec2 = [3,1,4]
+        vecp = [3,-2,4]
+        ans = [87/155,-16/155,166/155]
+        hat = reflect_point(vec1, vec2, vecp)
+        for i in range(3):
+            self.assertAlmostEqual(ans[0], hat[0])
 
 if __name__ == '__main__':
     unittest.main()
