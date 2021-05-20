28277de889f9ac29f6c2565dee579b4042e872cc
Matthew Truscott <matthew.a.truscott@gmail.com>
2021-05-20 12:31:50 -0500

    setup and tests

--- tests/test_adsorbate_gen_multitype.py
+++ tests/test_adsorbate_gen_multitype.py
@@ -1,5 +1,5 @@
 import unittest
-from aiida_environ.calculations.adsorbate_calc import adsorbate_calculation
+from aiida_environ.calculations.adsorbate.gen_multitype import adsorbate_gen_multitype
 
 class TestAdsorbate(unittest.TestCase):
     def count_species(self, occ_list):
@@ -14,17 +14,19 @@ class TestAdsorbate(unittest.TestCase):
         return count
 
     def test_single_simple(self):
+        print("\ntest_single_simple")
         site_index = [0, 0]
         possible_adsorbates = ['H']
         adsorbate_index = [[1]]
-        max_list = adsorbate_calculation(site_index, possible_adsorbates, adsorbate_index)
+        max_list = adsorbate_gen_multitype(site_index, possible_adsorbates, adsorbate_index)
         self.assertEqual(max_list, [[[0, 'H']]])
         
     def test_single_complex(self):
+        print("\ntest_single_complex")
         site_index = [0, 0, 0, 0]
         possible_adsorbates = ['H', 'O']
         adsorbate_index = [[1, 1]]
-        max_list = adsorbate_calculation(site_index, possible_adsorbates, adsorbate_index)
+        max_list = adsorbate_gen_multitype(site_index, possible_adsorbates, adsorbate_index)
         ref_count = {
             0: 4,
             'H': 4,
@@ -33,17 +35,19 @@ class TestAdsorbate(unittest.TestCase):
         self.assertDictEqual(self.count_species(max_list), ref_count)
 
     def test_multi_simple(self):
+        print("\ntest_multi_simple")
         site_index = [0, 1]
         possible_adsorbates = ['H']
         adsorbate_index = [[1], [1]]
-        max_list = adsorbate_calculation(site_index, possible_adsorbates, adsorbate_index)
+        max_list = adsorbate_gen_multitype(site_index, possible_adsorbates, adsorbate_index)
         self.assertEqual(len(max_list), 4)
     
     def test_multi_complex(self):
+        print("\ntest_multi_complex")
         site_index = [0, 0, 1, 1, 2]
         possible_adsorbates = ['H', 'OH', 'O']
         adsorbate_index = [[1, 1, 0], [1, 0, 0], [1, 1, 1]]
-        max_list = adsorbate_calculation(site_index, possible_adsorbates, adsorbate_index)
+        max_list = adsorbate_gen_multitype(site_index, possible_adsorbates, adsorbate_index)
         print(max_list)
         print(self.count_species(max_list))
 
---
+++ tests/test_adsorbate_gen_supercell.py
@@ -0,0 +1,6 @@
+import unittest
+class TestAdsGen(unittest.TestCase):
+    def test_(self):
+
+if __name__ == '__main__':
+    unittest.main()
