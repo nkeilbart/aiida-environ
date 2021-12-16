9b49e04400c94bb8728674dda6fa35bee109ba88
Matthew Truscott <matthew.a.truscott@gmail.com>
2021-12-16 15:55:50 -0600

    redo of examples stage 1

---
+++ examples/ex_environ_calc.py
@@ -0,0 +1,26 @@
+from aiida import load_profile
+load_profile()
+
+from aiida.orm.utils import load_code
+from aiida.engine import submit
+from aiida.orm.nodes.data.upf import get_pseudos_from_structure
+
+from aiida_quantumespresso.utils.resources import get_default_options
+
+from make_inputs import *
+
+# try loading aiida-environ, everything stored as nodes already
+code = load_code(5714)
+builder = code.get_builder()
+builder.metadata.label = "environ example"
+builder.metadata.description = "environ.pw calcjob"
+builder.metadata.options = get_default_options()
+
+builder.structure = make_organic_structure()
+builder.kpoints = make_simple_kpoints()
+builder.parameters = make_simple_parameters()
+builder.pseudos = get_pseudos_from_structure(builder.structure, 'SSSPe')
+builder.environ_parameters = make_simple_environ_parameters()
+
+calculation = submit(builder)
+print(calculation)
