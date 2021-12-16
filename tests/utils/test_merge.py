800cc1d8d8472d2236dd326c9a23f9163c298417
Matthew Truscott <matthew.a.truscott@gmail.com>
2021-12-16 15:02:35 -0600

    updated tests to pytest

--- tests/utils/test_merge.py
+++ tests/utils/test_merge.py
@@ -1,63 +1,66 @@
-import unittest
+import pytest
+
 from copy import deepcopy
 from aiida_environ.utils.merge import recursive_update_dict
 
-class TestUpdateDict(unittest.TestCase):
-    def __init__(self, *args, **kwargs):
-        super(TestUpdateDict, self).__init__(*args, **kwargs)
-        self.d = {
-            0: {
-                0: 2,
-                1: 4,
-                2: 3
-            }, 
-            1: {
-                0: 3,
-                1: 5,
-                2: 1
-            }
+
+def get_test_dict() -> dict:
+    d = {
+        0: {
+            0: 2,
+            1: 4,
+            2: 3
+        }, 
+        1: {
+            0: 3,
+            1: 5,
+            2: 1
         }
+    }
+    return d
 
-    def test_add_inner(self):
-        oldd = deepcopy(self.d)
-        refd = deepcopy(self.d)
-        newd = {
-            0: {
-                3: 2
-            }
+
+def test_add_inner():
+    d = get_test_dict()
+    oldd = deepcopy(d)
+    refd = deepcopy(d)
+    newd = {
+        0: {
+            3: 2
         }
-        refd[0][3] = 2
+    }
+    refd[0][3] = 2
 
-        recursive_update_dict(oldd, newd)
-        self.assertDictEqual(oldd, refd)
+    recursive_update_dict(oldd, newd)
+    assert oldd == refd
 
-    def test_replace_inner(self):
-        oldd = deepcopy(self.d)
-        refd = deepcopy(self.d)
-        newd = {
-            0: {
-                2: 1
-            }
+
+def test_replace_inner():
+    d = get_test_dict()
+    oldd = deepcopy(d)
+    refd = deepcopy(d)
+    newd = {
+        0: {
+            2: 1
         }
-        refd[0][2] = 1
+    }
+    refd[0][2] = 1
 
-        recursive_update_dict(oldd, newd)
-        self.assertDictEqual(oldd, refd)
+    recursive_update_dict(oldd, newd)
+    assert oldd == refd
 
-    def test_add_outer(self):
-        oldd = deepcopy(self.d)
-        refd = deepcopy(self.d)
-        newd = {
-            3: {
-                2: 1
-            }
+
+def test_add_outer():
+    d = get_test_dict()
+    oldd = deepcopy(d)
+    refd = deepcopy(d)
+    newd = {
+        3: {
+            2: 1
         }
-        refd[3] = {}
-        refd[3][2] = 1
+    }
+    refd[3] = {}
+    refd[3][2] = 1
 
-        recursive_update_dict(oldd, newd)
-        self.assertDictEqual(oldd, refd)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    recursive_update_dict(oldd, newd)
+    assert oldd == refd
