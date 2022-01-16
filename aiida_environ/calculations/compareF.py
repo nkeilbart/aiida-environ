a5499f5eb5983116c8ce7222ff5b2b825828817c
Nicholas Martinez <nicholas@macbook.home>
2022-01-15 22:51:25 -0600

    added input & gate for interpolation types; added move_list input -- test restarted and rerun for each indexed atom; added pseudo validation using aiida-pseudo (to be the new standard)

--- aiida_environ/calculations/compareF.py
+++ aiida_environ/calculations/compareF.py
@@ -2,11 +2,12 @@ from aiida.orm.utils import load_node
 from aiida.engine import calcfunction
 
 @calcfunction
-def compare_forces(pks, atom, axis):
+def compare_forces(interpolation, pks, atom, axis):
 
     '''Compare DFT total force to numerical derivative dE/dx.
     
     Args:
+        difftype: aiida.orm.Str
         pks: aiida.orm.List
         atom: aiida.orm.Int
         axis: aiida.orm.Int
@@ -15,24 +16,33 @@ def compare_forces(pks, atom, axis):
         difference: list
     '''
 
-    fin_Fs = []
     dft_Fs = []
+    fin_Fs = []
     differences = []
 
-    for i in range(1, len(pks)-1):
+    for i in range(len(pks)):
 
         ith = load_node(pks[i])     # ith-calculation
         prec = load_node(pks[i-1])  # preceding calculation
         fllw = load_node(pks[i+1])  # following calculation
         
+        ith_coord = prec.inputs.structure.sites[atom].position[axis]
         prec_coord = prec.inputs.structure.sites[atom].position[axis]
         foll_coord = fllw.inputs.structure.sites[atom].position[axis]
 
-        dE = (fllw.res.energy - prec.res.energy) / (foll_coord - prec_coord)
+        if interpolation == 'central':
+            if i > 0 and i < len(pks)-1: dE = (fllw.res.energy - prec.res.energy) / (foll_coord - prec_coord)
+        elif interpolation == 'forward':
+            if i < len(pks)-1: dE = (fllw.res.energy - ith.res.energy) / (foll_coord - ith_coord)
+        elif interpolation == 'backward':
+            if i > 0: dE = (ith.res.energy - prec.res.energy) / (ith_coord - prec_coord)
+
         F = ith.res.total_force
 
         fin_Fs.append(dE)
         dft_Fs.append(F)
         differences.append(abs(F - dE))
+
+        # TODO ADD HIGHER ORDER DIFFERENTIATION
 
     return differences # TODO return DFT Fs & numerical Fs?
--- aiida_environ/workflows/pw/testF.py
+++ aiida_environ/workflows/pw/testF.py
@@ -1,16 +1,16 @@
 from aiida.engine import BaseRestartWorkChain, WorkChain
 from aiida.plugins import WorkflowFactory, CalculationFactory
-from aiida.orm import StructureData, Dict, List, Int, load_node
+from aiida.orm import StructureData, Dict, List, Int, Str, load_node, load_group
 
 from aiida_quantumespresso.utils.mapping import prepare_process_inputs
 from aiida_quantumespresso.workflows.protocols.utils import ProtocolMixin, recursive_merge
 
-#import random # TODO add randomizing option
+import random
 
 EnvPwBaseWorkChain = WorkflowFactory('environ.pw.base')
 
 class ForceTestWorkChain(EnvPwBaseWorkChain):
-
+    
     # TODO default block here?
 
     @classmethod
@@ -32,24 +32,70 @@ class ForceTestWorkChain(EnvPwBaseWorkChain):
 
         '''Setup default structure & parameters for testing. -- in progress'''
 
-        # FIXME need to check inputs
+        # load environ parameters
+        self.ctx.environ_parameters = self.inputs.base.pw.environ_parameters
+
+        # local variables for setup
+        difftypes = ('forward', 'backward', 'central')           # interpolate type tuple
+        axes = ('x', 'y', 'z')                                   # axis tuple
+        nat = self.inputs.base.pw.parameters['CONTROL']['nat']   # set nat for random
+        wild = random.randint(1, nat)                            # assign random index
 
         # 2 Si atoms - aiida-quantumespresso/tests/calculations/test_pw/test_default_pw.in
-        cell = [[2.715, 0, 2.715], [0, 2.715, 2.715], [2.715, 2.715, 0]]
-        structure = StructureData(cell=cell)
-        structure.append_atom(position=(0., 0., 0.), symbols='Si', name='Si')
-        structure.append_atom(position=(1.3575, 1.3575, 1.3575), symbols='Si', name='Si')
-        self.pw.structure = structure
+        # FIXME not sure how to validate structure before base.pw excepts - don't want to load every time
+        if self.inputs.base.pw['structure'] is None:
+            cell = [[2.715, 0, 2.715], [0, 2.715, 2.715], [2.715, 2.715, 0]]
+            structure = StructureData(cell=cell)
+            structure.append_atom(position=(0., 0., 0.), symbols='Si', name='Si')
+            structure.append_atom(position=(1.3575, 1.3575, 1.3575), symbols='Si', name='Si')
+            self.pw.structure = structure
 
-        # FIXME require user to enter pseudo? -- look for pseudo group; use get_pseudos(structure=structure)
-        
-        self.ctx.environ_parameters = self.inputs.base.pw.environ_parameters
+        # validate pseudo string
+        upfstr = self.inputs.test_parameters.pseudo
+        try:
+            upf = load_group(upfstr)
+            self.inputs.base['pseudos'] = upf.get_pseudos(structure=self.inputs.base.pw['structure'])
+        except:
+            raise NameError(f'{upfstr} is not an imported pseudo family. Make sure to use aiida-pseudo plugin')
+
+        # get user-input test parameters and set defaults
         chain_parameters = self.inputs.test_parameters.get_dict()
         chain_parameters.setdefault('multi', False)               # default move one atom at a time
-        chain_parameters.setdefault('perturbed', 0)               # default first atom moved #TODO change to tuple for multiple-atom translation; random option
+        chain_parameters.setdefault('diff_type', 'central')       # default central difference interpolation
         chain_parameters.setdefault('nsteps', 5)                  # default n = 5
-        chain_parameters.setdefault('axis', 0)                    # default x-direction #TODO change to tuple for multiple-atom translation; random option
+        chain_parameters.setdefault('axis', wild)                 # default random direction (int)
         chain_parameters.setdefault('step', 0.1)                  # default dx = 0.1
+        chain_parameters.setdefault(                              # default one random atom moved
+            'move_list',
+            [random.randint(1, nat)]
+        )
+
+        # validate user-input axis string
+        axstr = self.inputs.test_parameters.axis
+        if axstr in axes:
+            self.inputs.test_parameters.axis = axes.index(axstr)
+        else: # set default for garbage input
+            print('\nAxis selection not valid. Setting to random axis')
+            self.inputs.test_parameters.axis = wild
+        self.ctx.axstr = axstr # useful for calc descriptions
+        
+        # validate user-input interpolation type
+        typestr = self.inputs.test_parameters.diff_type
+        if typestr in difftypes:
+            self.inputs.test_parameters.diff_type = typestr
+        else: # set default for garbage input
+            print(f'{typestr} is not valid. Setting to central difference interpolation')
+            self.inputs.test_parameters.diff_type = 'central'
+
+        # validate user-input tuple of atoms moved
+        index_list = self.inputs.test_parameters.move_list
+        valid_indices = *(int(i) for i in range(1, nat+1)),
+        for index in index_list:
+            if index not in valid_indices: # set default for garbage input
+                print(f'\n{index} is not a valid atom index. Setting to first atom')
+                self.inputs.test_parameters.move_list = (0)
+                break
+
         self.inputs.test_parameters = Dict(dict=chain_parameters)
 
     def run_test(self):
@@ -57,125 +103,139 @@ class ForceTestWorkChain(EnvPwBaseWorkChain):
         '''Displace an atom from initial position to compare forces. -- needs testing'''
 
         # test parameter variable block for logic legibility
-        n = self.inputs.test_parameters.nsteps              # total number of steps
-        atom = self.inputs.test_parameters.perturbed        # index of atom perturbed
-        axis = self.inputs.test_parameters.axis             # index of axis to perturb
-        step = self.inputs.test_parameters.step             # step size
-        multi = self.inputs.test_parameters.multi           # multi-atom perturb flag
+        n = self.inputs.test_parameters.nsteps                  # total number of steps
+        atoms = self.inputs.test_parameters.move_list           # tuple of ith atoms to move
+        axis = self.inputs.test_parameters.axis                 # index of axis to perturb
+        step = self.inputs.test_parameters.step                 # step size
+        multi = self.inputs.test_parameters.multi               # multi-atom perturb flag
 
         if multi:
             prefix = 'multi'
-        else:
-            prefix = 'single'
 
-        # identifier variables
-        axnames = ['x', 'y', 'z'] # axis strings
-        displacement = (0., 0.) # displaced atom tuple # TODO make dynamic
+        # calculation dictionary -- len(calculations) = len(move_list)
+        # calculations = {
+        #   1 : [calcPK1, calcPK2, calcPK3, ..., calcPKn],
+        #   ...,
+        #   # : calclist
+        # }
+        self.ctx.calculations = {}
 
-        for i in range(n):
+        # perform test for each atom in move_list
+        for atom in atoms:
+        
+            atom -= 1 # ith atom has index i-1
+            
+            if not multi:
+                prefix = f'atom{atom}'
 
-            if i == 0: # initial calculation
+            for i in range(n):
 
-                # calculation identifiers
-                self.pw.metadata.description = 'Initial structure | {}'.format(displacement) # brief description
-                name = f'{prefix}.{axnames[axis]}.0' # calculation node key in context dictionary
+                if i == 0: # initial calculation
 
-            else: # displaced atom calculations
+                    # calculation identifiers
+                    self.inputs.pw.metadata.description = 'Initial structure | d{} = {}'.format(self.ctx.axstr, i * step) # brief description
+                    name = f'{prefix}.{axis}.0' # calculation node key in context dictionary
 
-                # get initial calculation data
-                if i == 1: # only structure will change between calculations
+                else: # displaced atom calculations
 
-                    initname = f'{prefix}.{axnames[axis]}.0' # assign previous calculation name
-                    initcalc = load_node(self.ctx.calculations[initname]) # load previous calculation node
+                    # get initial calculation data
+                    if i == 1: # only structure will change between calculations
 
-                    code = initcalc.inputs.base.pw.code
-                    pseudos = initcalc.inputs.base.pw.pseudos
-                    kpoints = initcalc.inputs.base.pw.kpoints
-                    parameters = initcalc.inputs.base.pw.environ_parameters
-                    settings = initcalc.inputs.base.settings
+                        initname = f'{prefix}.{self.ctx.axstr}.0' # assign previous calculation name
+                        initcalc = load_node(self.ctx.calculations[initname]) # load previous calculation node
 
-                    cell = self.initcalc.inputs.structure.cell # initial structure unit cell
-                    sites = self.initcalc.inputs.structure.sites # initial structure position tuple; AiiDA Site objects
-                    edge = self.initcalc.inputs.structure.cell_lengths[atom] # max cell length
+                        code = initcalc.inputs.base.pw.code
+                        pseudos = initcalc.inputs.base.pw.pseudos
+                        kpoints = initcalc.inputs.base.pw.kpoints
+                        parameters = initcalc.inputs.base.pw.environ_parameters
+                        settings = initcalc.inputs.base.settings
 
-                # perturb atom
-                displacement[atom] = i * step # update displacement tuple
-                position = sites[atom].position # initial atom position
-                newposition = (0., 0., 0.)
-                for j in range(len(position)): # build new position tuple
-                    if j == axis:
-                        newposition[axis] = position[axis] + i * step
+                        cell = self.initcalc.inputs.structure.cell # initial structure unit cell
+                        sites = self.initcalc.inputs.structure.sites # initial structure position tuple; AiiDA Site objects
+                        edge = self.initcalc.inputs.structure.cell_lengths[atom] # max cell length
+
+                    # perturb atom
+                    position = sites[atom].position # initial atom position
+                    newposition = (0., 0., 0.)
+                    for j in range(len(position)): # build new position tuple
+                        if j == axis:
+                            newposition[axis] = position[axis] + i * step
+                        else:
+                            newposition[j] = position[j]
+                    
+                    sites[atom].position = newposition # update local position tuple
+
+                    # build new calculation
+                    if (edge - position[0]) > 0.001: # check if atom is still in cell before calculation
+        
+                        # builder initialization
+                        builder = EnvPwBaseWorkChain.get_builder()
+                        builder.pw.code = code
+                        builder.pw.pseudos = pseudos
+                        builder.pw.kpoints = kpoints
+                        builder.parameters = parameters
+                        builder.settings = settings
+                        builder.metadata.description = 'Perturbed structure | {}'.format(i * step)
+                        
+                        name = f'{prefix}.{self.ctx.axstr}.{i * step}'
+
+                        # add structure to builder
+                        structure = StructureData(cell=cell)
+                        for j in range(len(sites)):
+                            structure.append_atom(position=sites[j].position, symbols=sites[j].kind_name)
+                        builder.structure = structure
+                        
                     else:
-                        newposition[j] = position[j]
-                
-                sites[atom].position = newposition # update local position tuple
 
-                # build new calculation
-                if (edge - position[0]) > 0.001: # check if atom is still in cell before calculation
-    
-                    # builder initialization
-                    builder = EnvPwBaseWorkChain.get_builder()
-                    builder.pw.code = code
-                    builder.pw.pseudos = pseudos
-                    builder.pw.kpoints = kpoints
-                    builder.parameters = parameters
-                    builder.settings = settings
-                    builder.metadata.description = 'Perturbed structure | {}'.format(displacement)
-                    
-                    name = f'{prefix}.{axnames[axis]}.{i * step}'
+                        print('\nNew atom position appears to be outside cell bounds. Stopping.')
+                        quit()
 
-                    # add structure to builder
-                    structure = StructureData(cell=cell)
-                    for j in range(len(sites)):
-                        structure.append_atom(position=sites[j].position, symbols=sites[j].kind_name)
-                    builder.structure = structure
-                    
-                else:
+                # submit calculations in parallel
+                calc = self.submit(EnvPwBaseWorkChain, **self.ctx.environ_parameters)
+                self.to_context(**{name: calc})
 
-                    print('New atom position may be outside cell bounds')
-                    quit()
-
-            # submit calculations in parallel
-            calc = self.submit(EnvPwBaseWorkChain, **self.ctx.environ_parameters)
-            self.to_context(**{name: calc})
-
-        # collect calculation pks using key convention for next step
-        self.ctx.calclist = [self.ctx[f'{prefix}.{axnames[axis]}.{k}'].pk for k in range(n)]
+            # collect the calculation pks for this atom's test series using key convention
+            self.ctx.calculations[atom+1] = [self.ctx[f'{prefix}.{self.ctx.axstr}.{k}'].pk for k in range(n)]
 
     def display_results(self): # quantitative (chart) & qualitative (plot)
 
         '''Compare finite difference forces against DFT forces -- needs testing'''
 
-        calcs = self.ctx.calclist                           # list of calc pks
-        atom = self.inputs.test_parameters.perturbed        # index of atom perturbed
-        axis = self.inputs.test_parameters.axis             # index of axis to perturb
+        diff_type = self.inputs.test_parameters.diff_type       # interpolation type string
+        atoms = self.inputs.test_parameters.move_list           # index of atom perturbed
+        axis = self.inputs.test_parameters.axis                 # index of axis to perturb
+        step = self.inputs.test_parameters.step                 # step size float
 
         CompareCalculation = CalculationFactory('environ.compareF')
-        results = CompareCalculation(List(list=calcs), Int(atom), Int(axis))
+        results = {}
+        for atom in atoms:
 
-        # calculation parameters
-        print()
-        print(f'atom number = {atom+1}')
-        print(f'axis        = {axis}     ( 0-x | 1-y | 2-z )')
-        print(f'dh          = {self.inputs.test_parameters.step}')
-        #print(f'environ     = {use_environ}')
-        #print(f'doublecell  = {double_cell}')
+            calclist = self.ctx.calculations[atom]
+            results[atom] = CompareCalculation(Str(diff_type), List(list=calclist), Int(atom), Int(axis))
 
-        print(results)
+            # calculation parameters
+            print()
+            print(f'atom number = {atom}')
+            print(f'axis        = {self.ctx.axstr}')
+            print(f'd{self.ctx.axstr}          = {step}')
+            #print(f'environ     = {use_environ}')
+            #print(f'doublecell  = {double_cell}')
 
-        # header
-        #print('\ncoord    energy       force      fd     err')
+            print(results) # FIXME ADD DISPLAY FORMATTING
 
-        # display results
-        #for i in range(len(results)):
-        #
-        #    coord = init_coord + dh * i
-        #    print('{:5.2f} {:12.8f}{:12.8f}'.format(coord, *results[i]), end=' ')
-        #
-        #    if 0 < i < len(results) - 1:
-        #        f, fd = compute_fd(results, i)
-        #        print(f'{fd:6.3f} {abs(fd - f):2.2e}', end=' ')
-        #
-        #    print()
+            # header
+            #print('\ncoord    energy       force      fd     err')
+
+            # display results
+            #for i in range(len(results)):
+            #
+            #    coord = init_coord + dh * i
+            #    print('{:5.2f} {:12.8f}{:12.8f}'.format(coord, *results[i]), end=' ')
+            #
+            #    if 0 < i < len(results) - 1:
+            #        f, fd = compute_fd(results, i)
+            #        print(f'{fd:6.3f} {abs(fd - f):2.2e}', end=' ')
+            #
+            #    print()
 
         return
