5f5cef261cfdc4c4a0e16883c92cc8721f32f124
Nicholas Martinez <nicholasmartinez@my.unt.edu>
2022-01-24 17:03:00 -0600

    finite difference workchain runs; testing finite diff calcfunction

--- aiida_environ/workflows/pw/compare_forces.py
+++ aiida_environ/workflows/pw/compare_forces.py
@@ -1,8 +1,9 @@
-from aiida.orm import StructureData, Dict, List, Float, Str, load_node, load_group
+from aiida.orm import StructureData, QueryBuilder, Dict, List, Float, Str, load_group
 from aiida.engine import WorkChain
 
-from aiida_environ.workflows.pw.base import EnvPwBaseWorkChain
 from aiida_environ.calculations.finite import calculate_finite_differences
+from aiida_environ.workflows.pw.base import EnvPwBaseWorkChain
+from aiida_pseudo.groups.family.pseudo import PseudoPotentialFamily
 
 import random
 
@@ -45,227 +46,71 @@ class FiniteForcesWorkChain(WorkChain):
 
         spec.outline(
             cls.setup,
-            cls.validate_test_settings,
             cls.run_test,
             cls.display_results
         )
 
-    def _validate_diff_type(cls, self):
-        """-1"""
-
-        type_str = self.inputs.test_settings['diff_type']
-
-        # type validation
-        if not isinstance(type_str, str):
-            raise Exception("\ndiff_type must be 'forward', 'backward', or 'central'")
-
-        # string validation
-        if type_str in cls.types:
-            self.inputs.test_settings.diff_type = type_str        
-        else:
-            print(f'\n{type_str} is not valid. Setting to central difference interpolation')
-            self.inputs.test_settings.diff_type = 'central'
-
-    def _validate_diff_order(cls, self):
-
-        ord_str = self.inputs.test_settings['diff_order']
-
-        # type validation        
-        if not isinstance(ord_str, str):
-            raise Exception("\ndiff_order must be 'first' or 'second'")
-
-        if ord_str in cls.orders:
-            self.inputs.test_settings.diff_order = ord_str
-        else:
-            print(f'{ord_str} is not valid. Setting to first-order finite difference')
-            self.inputs.test_settings.diff_order = 'first'
-
-    def _validate_step_sizes(cls, self):
-        """-1"""
-
-        steplist = self.inputs.test_settings['step_sizes']
-
-        # type validation
-        if not isinstance(steplist, list):
-            raise Exception('\nstep_sizes must be a list of 3 floats')
-
-        # length validation
-        if len(steplist) != 3:
-            raise Exception('\nAxis tuple must have 3 elements: [dx, dy, dz]')
-
-        # element type validation
-        for dh in steplist:
-            if not isinstance(dh, float): raise Exception(f'\nStep list may only contain float values')
-
-        # magnitude validation
-        if steplist.count(0.0) == 1:
-            self.ctx.axstr = cls.axes[steplist.index(0.0)]
-        elif steplist.count(0.0) == 3: # set default for garbage input
-            print('\nStep size is 0. Setting to dx = 0.1')
-            self.inputs.test_settings['step_sizes'] = [0.01, 0.0, 0.0]
-            self.ctx.axstr = 'x'
-        else:
-            self.ctx.axstr = 'r'
-
-    def _validate_atom_to_move(self):
-        """-1"""
-
-        atom = self.inputs.test_settings['atom_to_move']
-
-        # type validation
-        if not isinstance(atom, int):
-            raise Exception('\natom_to_move must be an integer')
-
-        # magnitude validation
-        nat = len(self.inputs.structure.sites)
-        if atom < 0 or atom > nat:
-            raise Exception('\nAtom index must be greater than zero and less than number of atoms')
-
-    def _validate_nsteps(self):
-        """-1"""
-
-        # type validation
-        if not isinstance(self.inputs.nsteps, int):
-            raise Exception('\nnsteps must be an int')
-
-    def _prepare_new_inputs(self, i: int, dh: float) -> dict:
-        """Returns input dictionary ready for Process submission"""
-
-        inputs = {
-            'pw': {
-                'code': self.inputs.base.pw.code,
-                'structure': self._perturb_atom(),
-                'pseudos': self.inputs.base.pseudos,
-                'metadata': {
-                    'description': 'Perturbed structure | d{} = {:.2}'.format(self.ctx.axstr, i * dh)
-                }
-            },
-            'kpoints': self.inputs.base.pw.kpoints,
-            'environ_parameters': self.inputs.base.environ_parameters
-        }
-
-        return inputs
-
-    def _perturb_atom(self, atom: int, i: int, steps: list) -> StructureData:
-        """Returns StructureData with updated position"""
-    
-        new_position = (0., 0., 0.)
-
-        # update position tuple
-        for j in range(3):
-            
-            if steps[j] != 0.0:
-            
-                new_position[j] = self.ctx.initial_position[j] + i * steps[j]
-
-                if (self.ctx.edge - new_position[j]) > 0.001:
-                    continue
-                else:
-                    raise Exception('\nNew atom_to_move position appears to be outside cell bounds. Stopping.')
-
-            else:
-                new_position[j] = self.ctx.initial_position[j]
-
-        new_structure = StructureData(cell=self.ctx.cell)
-
-        # build new StructureData
-        for k in range(len(self.ctx.sites)):
-
-            if k == atom:
-                new_structure.append_atom_to_move(
-                    position=new_position,
-                    symbols=self.ctx.sites[k].kind_name
-                )
-            
-            else:
-                new_structure.append_atom_to_move(
-                    position=self.ctx.sites[k].position,
-                    symbols=self.ctx.sites[k].kind_name
-                )
-
-        return new_structure
-
     def setup(self):
-        '''Setup default structure & parameters for testing. -- testing needed'''
+        '''Setup default structure & parameters for testing.'''
 
-        nat = len(self.inputs.structure.sites)  # nat for random
-        wild = random.randint(1, nat)           # random index
+        natoms = len(self.inputs.structure.sites)  # nat for random
+        wild = random.randint(1, natoms)           # random index
 
-        # get user-input test parameters and set defaults
+        # set default settings for missing keys
         settings_dict = self.inputs.test_settings.get_dict()
-        settings_dict.setdefault('diff_type', 'central')            # default central difference
-        settings_dict.setdefault('diff_order', 'first')             # default first-order difference
-        settings_dict.setdefault('atom_to_move', wild)              # default random atom_to_move moved
-        settings_dict.setdefault('nsteps', 5)                       # default n = 5
-        settings_dict.setdefault('step_sizes', [0.1, 0.0, 0.0])     # default dr = dx = 0.1
+        settings_dict.setdefault('diff_type', 'forward')
+        settings_dict.setdefault('diff_order', 'first')
+        settings_dict.setdefault('atom_to_perturb', wild)
+        settings_dict.setdefault('nsteps', 5)
+        settings_dict.setdefault('step_sizes', [0.1, 0.0, 0.0])
+
+        # validate inputs
+        self._validate_pseudo_group()
+        self._validate_diff_type()
+        self._validate_diff_order()
+        self._validate_atom_to_perturb(natoms)
+        self._validate_nsteps(settings_dict['nsteps'])
+        self._validate_step_sizes()
 
         self.inputs.test_settings = Dict(dict=settings_dict)
 
-    def validate_test_settings(self): # TODO move validation blocks to /utils/validate.py?
-
-        '''Validate test test_settings with exception handling. -- testing needed'''
-
-        # local variables for validation # TODO add validation for atom_to_move & nsteps?
-
-        upf = load_group(self.inputs.pseudo_group)
-        self.inputs.base.pw.structure = self.inputs.structure
-        self.inputs.base.pseudos = upf.get_pseudos(structure=self.inputs.base.pw.structure)
-
-        self._validate_diff_type()
-        self._validate_diff_order()
-        self._validate_atom_to_move()
-        self._validate_nsteps()
-        self._validate_step_sizes()
-
     def run_test(self):
-        '''Displaces an atom_to_move from initial position, according to input test test_settings. -- needs testing'''
+        '''Displaces an atom_to_perturb from initial position, according to input test test_settings.'''
 
         # local variable block
-        atom = self.inputs.test_settings['atom_to_move'] - 1    # ith atom_to_move to move has index i-1
         diff_type = self.inputs.test_settings['diff_type']      # difference type
         steps = self.inputs.test_settings['step_sizes']         # list of step sizes
         n = self.inputs.test_settings['nsteps'] + 1             # initial position + n-perturbations
+        prefix = f'atom{self.atom}'
 
-        self.ctx.prefix = f'atom{atom}'
+        # context variable block
+        self.ctx.cell = self.inputs.structure.cell
+        self.ctx.edge = self.inputs.structure.cell_lengths[self.atom]
+        self.ctx.sites = self.inputs.structure.sites
+        self.ctx.initial_position = self.ctx.sites[self.atom].position
 
-        if diff_type == 'central': n *= 2 # central difference requires half-step calculations
+        # central difference requires half-step increments
+        if diff_type == 'central':
+            n *= 2
+            step = sum([(dh/2) ** 2 for dh in steps]) ** 0.5
+        else:
+            step = sum([dh ** 2 for dh in steps]) ** 0.5
 
+        # submit calculations
         for i in range(n):
 
-            # central difference requires half-step increments
-            if diff_type == 'central':
-
-                step = sum([(dh/2) ** 2 for dh in steps]) ** 0.5
-            
-            else:
-            
-                step = sum([dh ** 2 for dh in steps]) ** 0.5
-
-            if i == 0: # initial calculation
-
-                calcname = f'{self.ctx.prefix}.{self.ctx.axis}.0'
-                self.inputs.base.pw['metadata']['description'] = \
-                    'Initial structure | d{} = {:.2}'.format(self.ctx.axstr, i * step)
-
-                calc = self.submit(EnvPwBaseWorkChain, **self.inputs.base)
-
-                # assign data needed to context
-                self.ctx.cell = self.inputs.structure.cell
-                self.ctx.edge = self.inputs.structure.cell_lengths[atom]
-                self.ctx.sites = self.inputs.structure.sites
-                self.ctx.initial_position = self.ctx.sites[atom].position
-
-            else: # perturbed atom calculation
-
-                calcname = f'{self.ctx.prefix}.{self.ctx.axstr}.{i}'
-                inputs = self._prepare_builder(i, i * step)
-                
-                calc = self.submit(EnvPwBaseWorkChain, **inputs)
+            calcname = f'{prefix}.{self.ctx.axstr}.{i}'
+            inputs = self._prepare_inputs(i, i * step)
+            calc = self.submit(EnvPwBaseWorkChain, **inputs)
 
             self.to_context(**{calcname: calc})
 
-        # collect the calculation pks for this atom_to_move's test series using key convention
-        self.ctx.calculations = [self.ctx[f'{self.ctx.prefix}.{self.ctx.axstr}.{k}'].pk for k in range(n)]
+        # collect the calculation pks
+        self.ctx.calculations = []
+
+        for k in range(n):
+            name = f'{prefix}.{self.ctx.axstr}.{k}'
+            self.ctx.calculations.append(self.ctx[name].pk)
 
     def display_results(self): # quantitative (chart) & qualitative (plot)
 
@@ -273,7 +118,6 @@ class FiniteForcesWorkChain(WorkChain):
 
         diff_type = self.inputs.test_settings['diff_type']       # difference type string
         diff_order = self.inputs.test_settings['diff_order']     # difference order string
-        atom = self.inputs.test_settings['atom_to_move']         # index of atom_to_move perturbed
         steps = self.inputs.test_settings['step_sizes']          # list of steps
         step = sum([dh ** 2 for dh in steps]) ** 0.5             # step size -- same for all difference types
 
@@ -287,7 +131,7 @@ class FiniteForcesWorkChain(WorkChain):
 
         # calculation parameters
         print()
-        print(f'atom number = {atom}')
+        print(f'atom number = {self.atom}')
         print(f'axis        = {self.ctx.axstr}')
         print('d{}          = {}'.format(self.ctx.axstr, step))
         #print(f'environ     = {use_environ}')
@@ -311,3 +155,179 @@ class FiniteForcesWorkChain(WorkChain):
         #    print()
 
         return
+
+    def _prepare_inputs(self, i: int, dr: float) -> dict:
+        """Returns input dictionary ready for Process submission"""
+
+        if i == 0:
+            which = 'Initial'
+        else:
+            which = 'Perturbed'
+
+        inputs = {
+                'pw': {
+                    'code': self.inputs.base.pw.code,
+                    'pseudos': self.ctx.pseudos,
+                    'parameters': self.inputs.base.pw.parameters,
+                    'environ_parameters': self.inputs.base.pw.environ_parameters,
+                },
+                'metadata': {
+                    'description': f"{which} structure | d{self.ctx.axstr} = {dr:.2f}",
+                    'resources': {
+                        'num_machines': 1,
+                        'num_mpiprocs_per_machine': 4
+                    }
+                },
+                'kpoints': self.inputs.base.kpoints
+            }
+
+        if i == 0:
+            inputs['pw']['structure'] = self.inputs.structure
+        else:
+            inputs['pw']['structure'] = self._perturb_atom(
+                        atom = self.atom,
+                        i = i,
+                        steps = self.inputs.test_settings['step_sizes']
+                    )
+
+        return inputs
+
+    def _perturb_atom(self, atom: int, i: int, steps: list) -> StructureData:
+        """Returns StructureData with updated position"""
+    
+        new_position = [0., 0., 0.]
+
+        # update position tuple
+        for j in range(3):
+            
+            if steps[j] != 0.0:
+            
+                new_position[j] = self.ctx.initial_position[j] + i * steps[j] # FIXME ATOM BEING DISPLACED AT (i**2) * step
+
+                if (self.ctx.edge - new_position[j]) > 0.001:
+                    continue
+                else:
+                    raise Exception('\nNew atom_to_perturb position appears to be outside cell bounds. Stopping.')
+
+            else:
+                new_position[j] = self.ctx.initial_position[j]
+
+        new_structure = StructureData(cell=self.ctx.cell) # NEW STRUCTURE HAS 
+
+        # TODO index existing StructureData for one-to-one Site replacement?
+        # build new StructureData
+        for k in range(len(self.ctx.sites)):
+
+            if k == atom:
+                new_structure.append_atom(
+                    position=new_position,
+                    symbols=self.ctx.sites[k].kind_name
+                )
+            
+            else:
+                new_structure.append_atom(
+                    position=self.ctx.sites[k].position,
+                    symbols=self.ctx.sites[k].kind_name
+                )
+
+        return new_structure
+
+    def _validate_pseudo_group(self):
+        """Validates pseudopotential family input"""
+        
+        qb = QueryBuilder()
+        qb.append(
+            PseudoPotentialFamily,
+            project='label'
+        )
+
+        group = self.inputs.pseudo_group.value
+
+        # pseudo family validation
+        if group in qb.all(flat=True):
+            upf = load_group(group)
+        else:
+            print(f"\n{group} is not in aiida-pseudo families")
+            upf = load_group('SSSP/1.1/PBE/efficiency')
+
+        self.ctx.pseudos = upf.get_pseudos(structure=self.inputs.structure)
+
+    def _validate_diff_type(self):
+        """Validate finite difference type input"""
+
+        type_str = self.inputs.test_settings['diff_type']
+
+        # type validation
+        if not isinstance(type_str, str):
+            raise Exception("\ndiff_type must be 'forward', 'backward', or 'central'")
+
+        # string validation
+        if type_str in self.types:
+            self.inputs.test_settings.diff_type = type_str        
+        else:
+            print(f'\n{type_str} is not valid. Setting to central difference interpolation')
+            self.inputs.test_settings.diff_type = 'central'
+
+    def _validate_diff_order(self):
+        """Validates finite difference order input"""
+
+        ord_str = self.inputs.test_settings['diff_order']
+
+        # type validation        
+        if not isinstance(ord_str, str):
+            raise Exception("\ndiff_order must be 'first' or 'second'")
+
+        if ord_str in self.orders:
+            self.inputs.test_settings.diff_order = ord_str
+        else:
+            print(f'{ord_str} is not valid. Setting to first-order finite difference')
+            self.inputs.test_settings.diff_order = 'first'
+
+    def _validate_step_sizes(self):
+        """Validates step size input"""
+
+        steplist = self.inputs.test_settings['step_sizes']
+
+        # type validation
+        if not isinstance(steplist, list):
+            raise Exception('\nstep_sizes must be a list of 3 floats')
+
+        # length validation
+        if len(steplist) != 3:
+            raise Exception('\nAxis tuple must have 3 elements: [dx, dy, dz]')
+
+        # element type validation
+        for dh in steplist:
+            if not isinstance(dh, float): raise Exception(f'\nStep list may only contain float values')
+
+        # magnitude validation
+        if steplist.count(0.0) == 1:
+            self.ctx.axstr = self.axes[steplist.index(0.0)]
+        elif steplist.count(0.0) == 3: # set default for garbage input
+            print('\nStep size is 0. Setting to dx = 0.1')
+            self.inputs.test_settings['step_sizes'] = [0.01, 0.0, 0.0]
+            self.ctx.axstr = 'x'
+        else:
+            self.ctx.axstr = 'r'
+
+    def _validate_atom_to_perturb(self, nat):
+        """Validates atom index input"""
+
+        atom = self.inputs.test_settings['atom_to_perturb']
+
+        # type validation
+        if not isinstance(atom, int):
+            raise Exception('\natom_to_perturb must be an integer')
+
+        # magnitude validation
+        if atom < 0 or atom > nat:
+            raise Exception('\nAtom index must be greater than zero and less than number of atoms')
+
+        self.atom = atom - 1
+
+    def _validate_nsteps(self, nsteps):
+        """Validates total number of steps input"""
+
+        # type validation
+        if not isinstance(nsteps, int):
+            raise Exception('\nnsteps must be an int')
--- examples/ex_environ_force_test.py
+++ examples/ex_environ_force_test.py
@@ -4,7 +4,7 @@ from aiida.engine import submit
 from aiida import load_profile
 from aiida.orm import Dict, Str
 
-from aiida_environ.workflows.pw.compare_forces import ForceTestWorkChain
+from aiida_environ.workflows.pw.compare_forces import FiniteForcesWorkChain
 from aiida_quantumespresso.utils.resources import get_default_options
 
 from make_inputs import *
@@ -13,7 +13,7 @@ load_profile()
 sssp = load_group('SSSP/1.1/PBE/precision')
 
 # initialize force test workchain builder
-builder = ForceTestWorkChain.get_builder()
+builder = FiniteForcesWorkChain.get_builder()
 builder.metadata.label = "environ force test example"
 builder.metadata.description = "environ.pw force test workchain"
 
@@ -24,17 +24,14 @@ builder.base.kpoints = make_simple_kpoints()
 builder.base.pw.code = load_code(1)
 builder.base.pw.parameters = make_simple_parameters()
 builder.base.pw.environ_parameters = make_simple_environ_parameters()
-#builder.base.pw.structure = make_simple_structure()
-#builder.base.pw.pseudos = sssp.get_pseudos(structure=builder.base.pw.structure)
 
 # workchain setup
 builder.structure = make_simple_structure()
-builder.pseudo_group = Str('SSSP/1.1/PBE/precision')
 builder.test_settings = Dict(dict={
     'diff_type': 'central',
     'diff_order': 'first',
     'atom_to_move': 1,
-    'nsteps': 5,
+    'nsteps': 2,
     'step_sizes': [0.0, 0.1, 0.0]
 })
 
