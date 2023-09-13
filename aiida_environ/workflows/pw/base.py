464a2c01586afd5799b10a30bbad830668749f61
Keilbart <keilbart1@fireman.llnl.gov>
2023-09-13 07:20:30 -0700

    Update formatting

--- aiida_environ/workflows/pw/base.py
+++ aiida_environ/workflows/pw/base.py
@@ -393,7 +393,7 @@ class EnvPwBaseWorkChain(ProtocolMixin, BaseRestartWorkChain):
         pseudos = self.ctx.inputs.get("pseudos", {})
         if not pseudos:
             pseudo_family = self.inputs.get("pseudo_family", None)
-            self.report(pseudo_family.value)
+            self.report(f'{pseudo_family.value}')
 
             try:
                 pseudo_family = load_group(pseudo_family.value)
